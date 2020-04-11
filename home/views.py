353ad954fb7c697090df98c522fca301a0e48669
HasanSemiz <hasan.smz@hotmail.com>
2020-04-11 18:46:00 +0300

    ckeditör eklendi ve referanslar, hakkımızda, iletişim sayfaları oluşturuldu.

--- home/views.py
+++ home/views.py
@@ -7,5 +7,21 @@ from home.models import Setting
 
 def index(request):
     setting = Setting.objects.get(pk=1)
-    context = {'setting':setting}
+    context = {'setting':setting, 'page': 'home'}
     return render(request, 'index.html', context)
+
+
+def hakkimizda(request):
+    setting = Setting.objects.get(pk=1)
+    context = {'setting':setting,'page': 'hakkimizda'}
+    return render(request, 'hakkimizda.html', context)
+
+def referanslar(request):
+    setting = Setting.objects.get(pk=1)
+    context = {'setting':setting,'page': 'referanslar'}
+    return render(request, 'referanslar.html', context)
+
+def iletisim(request):
+    setting = Setting.objects.get(pk=1)
+    context = {'setting':setting,'page': 'iletisim'}
+    return render(request, 'iletisim.html', context)
