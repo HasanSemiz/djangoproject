74e4b10a4f2611d8e3123e7926941f59b47df638
HasanSemiz <hasan.smz@hotmail.com>
2020-05-10 16:29:01 +0300

    otomatik arama özelliği

--- home/views.py
+++ home/views.py
@@ -1,3 +1,4 @@
+import json
 from unicodedata import category
 
 from .forms import SearchForm
@@ -90,8 +91,12 @@ def product_search(request):
         if form.is_valid():
             category= Category.objects.all()
             query = form.cleaned_data['query']
-            products = Product.objects.filter(title__icontains=query)
+            catid = form.cleaned_data['catid']
 
+            if catid==0:
+               products = Product.objects.filter(title__icontains=query)
+            else:
+                products = Product.objects.filter(title__icontains=query, category_id=catid)
             context = {
                 'products': products,
                 'category': category,
@@ -101,3 +106,18 @@ def product_search(request):
             return render(request, 'products_search.html', context)
 
         return HttpResponseRedirect('/')
+
+def product_search_auto(request):
+  if request.is_ajax():
+    q = request.GET.get('term', '')
+    product = Product.objects.filter(title__icontains=q)
+    results = []
+    for rs in product:
+      product_json = {}
+      product_json = rs.title
+      results.append(product_json)
+    data = json.dumps(results)
+  else:
+    data = 'fail'
+  mimetype = 'application/json'
+  return HttpResponse(data, mimetype)
