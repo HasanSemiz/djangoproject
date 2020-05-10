b12d5fa8797e945d5a48a9e89228ae78f4e97d8d
HasanSemiz <hasan.smz@hotmail.com>
2020-05-10 21:55:54 +0300

    user log in log out

--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
 import json
 from unicodedata import category
 
+from django.contrib.auth import logout, authenticate, login
+
 from .forms import SearchForm
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
@@ -121,3 +123,27 @@ def product_search_auto(request):
     data = 'fail'
   mimetype = 'application/json'
   return HttpResponse(data, mimetype)
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect('/')
+
+def login_view(request):
+    if request.method == 'POST':
+        username= request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect('/')
+        else:
+            messages.warning(request, "Giriş Hatası! Kullanıcı adı veya şifre yanlış ")
+            return HttpResponseRedirect('/login')
+
+    category = Category.objects.all()
+    context = {
+        'category': category,
+    }
+
+    return render(request, 'login.html', context)
+
